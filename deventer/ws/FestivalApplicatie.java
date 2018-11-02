7e56b905895fecf8069dd499a50c8ea580294ed3
FelixvL <digitaleservice@gmail.com>
2018-11-02 16:28:45 +0100

    fix

--- deventer/ws/FestivalApplicatie.java
+++ deventer/ws/FestivalApplicatie.java
@@ -5,9 +5,7 @@ import deventer.ws.Podium.Podium1;
 
 public class FestivalApplicatie{
 	public static void main(String[] args) {
-		Kassa.getInstance().starten(50);
-		Kassa.getInstance().startKaartverkoop();
-//		Festival festival = new Festival();
-//		festival.organiseren();
+		Festival festival = new Festival();
+		festival.organiseren();
 	}
 }
