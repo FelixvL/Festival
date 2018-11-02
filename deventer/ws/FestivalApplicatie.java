07d05c55039c08c8f9ab17cde06dc4fb7347e930
FelixvL <digitaleservice@gmail.com>
2018-11-02 15:33:22 +0100

    festival festival

--- deventer/ws/FestivalApplicatie.java
+++ deventer/ws/FestivalApplicatie.java
@@ -5,6 +5,7 @@ import deventer.ws.Podium.Podium1;
 
 public class FestivalApplicatie{
 	public static void main(String[] args) {
-		new Festival().organiseren();
+		Festival festival = new Festival();
+		festival.organiseren();
 	}
 }
