d5ea4d5d296dccd88b5fbb6af967e791e7aa7d2c
Kevin Sweeren <sweerenk@gmail.com>
2018-11-02 15:57:59 +0100

    nieuwe functionalteit podium

--- deventer/ws/FestivalApplicatie.java
+++ deventer/ws/FestivalApplicatie.java
@@ -6,8 +6,8 @@ public class FestivalApplicatie{
 	public static void main(String[] args) {
 		System.out.println("gaaf");
 		new Podium1("Podium",500).starten();
-		new Artiest("Eminem").starten();
-		Kassa.getInstance().starten(50);
-		new Festival().organiseren();
+	//	new Artiest("Eminem").starten();
+	//	Kassa.getInstance().starten(50);
+	//	new Festival().organiseren();
 	}
 }
