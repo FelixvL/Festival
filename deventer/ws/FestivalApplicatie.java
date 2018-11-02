a3f46bbabcf23669bb7300ab255d1e8689bad1bc
jkadir1988 <jusreiza@gmail.com>
2018-11-02 14:38:20 +0100

    javieljus update 1

--- deventer/ws/FestivalApplicatie.java
+++ deventer/ws/FestivalApplicatie.java
@@ -1,7 +1,10 @@
 package deventer.ws;
 
+import deventer.ws.KassaSysteem.Kassa;
+
 public class FestivalApplicatie{
 	public static void main(String[] args) {
 		System.out.println("gaaf");
+		
 	}
 }
