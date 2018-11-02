f476118c8668d35a76f9dbde85a2563a4e5b83d0
FelixvL <digitaleservice@gmail.com>
2018-11-02 14:08:47 +0100

    start gemaakt main

--- deventer/ws/FestivalApplicatie.java
+++ deventer/ws/FestivalApplicatie.java
@@ -3,5 +3,9 @@ package deventer.ws;
 public class FestivalApplicatie{
 	public static void main(String[] args) {
 		System.out.println("gaaf");
+		new Podium().starten();
+		new Artiest("Eminem").starten();
+		Kassa.getInstance().starten(50);
+		new Festival.organiseren();
 	}
 }
