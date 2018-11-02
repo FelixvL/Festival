9d1be916b748788f99721154371c0f3df9a4fc70
Jorimvdw <jorimvanderwijngaard@gmail.com>
2018-11-02 15:22:03 +0100

    met random events

--- deventer/ws/FestivalApplicatie.java
+++ deventer/ws/FestivalApplicatie.java
@@ -2,10 +2,10 @@ package deventer.ws;
 
 public class FestivalApplicatie{
 	public static void main(String[] args) {
-		System.out.println("gaaf");
-		new Podium1("Podium",500).starten();
+	//	System.out.println("gaaf");
+	//	new Podium1("Podium",500).starten();
 		new Artiest("Eminem").starten();
-		Kassa.getInstance().starten(50);
-		new Festival().organiseren();
+	//	Kassa.getInstance().starten(50);
+	//	new Festival().organiseren();
 	}
 }
